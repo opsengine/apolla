8fc36b04d7229416077970e2dba918def49bc1a5
Angelo Marletta <angelo.marletta@gmail.com>
2012-01-18 03:24:20 +0000

    refactoring. arg parsing. tested

--- apolla.py
+++ apolla.py
@@ -1,29 +1,44 @@
+# Simple program which recovers lost picture from SD cards
+# Copyright (C) 2012  Angelo Marletta
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import sys
 import os
 import logging
-
-class FileReader:
-
-    def __init__(self, fd):
-        self.fd = fd
-
-    def read_byte(self):
-        return ord(self.fd.read(1))
+import argparse
 
 class ImageScanner:
 
-    def __init__(self, fd):
-        self.fd = fd
-        self.reader = FileReader(fd)
+    def __init__(self, device):
+        self.fd = open(device, "rb")
+        self.pointer = 0
+
+    def read_byte(self):
+        char = self.fd.read(1)
+        if char == "": raise Exception("EOF")
+        self.pointer += 1
+        return ord(char)
 
     def next_start_of_image(self):
         while (True):
-            if self.reader.read_byte() == 0xFF and self.reader.read_byte() == 0xD8:
+            if self.read_byte() == 0xFF and self.read_byte() == 0xD8:
                 return self.pointer - 2
         
     def next_end_of_image(self):
         while (True):
-            if self.reader.read_byte() == 0xFF and self.reader.read_byte() == 0xD9:
+            if self.read_byte() == 0xFF and self.read_byte() == 0xD9:
                 return self.pointer - 2
     
 class Apolla:
@@ -31,30 +46,25 @@ class Apolla:
     MAX_FILESIZE = 10*2**20
 
     def __init__(self, device):
-        self.device = open(device, "rb")
-        self.scanner = ImageScanner(device)
+        self.device = device
+        self.scanner = ImageScanner(self.device)
         self.devicesize = self.get_file_size(self.device)
 
     def recover(self):
         with open(self.device, "rb") as fd:
-            reader = Reader(self.device)
-            self.fd = fd
             for picture in self.find_pictures():
-                print picture
-                #self.export_file(picture)
+                self.export_file(picture[0], picture[1])
 
     def find_pictures(self):
         while (True):
             start_of_image = self.scanner.next_start_of_image()
             end_of_image = self.scanner.next_end_of_image()
-            if start_of_image == None or end_of_image == None:
-                break
             size = end_of_image + 2 - start_of_image
             if size <= Apolla.MAX_FILESIZE:
                 yield (start_of_image, size)
 
-    def export_file(self):
-        with open(self.filename, "rb") as input_file:
+    def export_file(self, start, size):
+        with open(self.device, "rb") as input_file:
             input_file.seek(start)
             bytes = input_file.read(size)
             output_filename = "%d.jpg" % start
@@ -63,15 +73,16 @@ class Apolla:
         print "File %s exported" % output_filename
 
     def get_file_size(self, filename):
-        "Get the file size by seeking at end"
-        fd= os.open(filename, os.O_RDONLY)
+        fd = os.open(filename, os.O_RDONLY)
         try:
             return os.lseek(fd, 0, os.SEEK_END)
         finally:
             os.close(fd)
 
-r = Apolla("/dev/mmcblk0p1")
-
-r.recover()
-
-r.close()
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Recovers logs pictures.')
+    parser.add_argument('--device', help='block device file')
+    args = parser.parse_args()
+    r = Apolla(args.device)
+    r.recover()
+    r.close()
